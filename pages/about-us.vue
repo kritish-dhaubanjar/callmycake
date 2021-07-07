37111daeadfbc45354e9c19c7e03fb6bc26b0015
PrijalBista <pri1.zzal@gmail.com>
2021-07-07 16:42:26 +0545

    [x] about us dynamic

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -7,34 +7,10 @@
         </div>
 
         <div class="col-12 my-5">
-          <img src="/images/about_us_banner_2_1296x.webp" class="img-fluid" />
+          <img :src="(aboutUs.image) ? `${$axios.defaults.baseURL}${aboutUs.image.path}`: '/images/about_us_banner_2_1296x.webp'" class="img-fluid" />
         </div>
 
-        <div class="col-12 mt-4">
-          <h4>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          </h4>
-          <p>
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque laudantium Sed ut perspiciatis unde omnis
-            iste natus error sit voluptatem accusantium doloremque laudantium,
-            On the other hand, we denounce with righteous indignation and
-            dislike men who are so beguiled and demoralized by the charms of
-            pleasure of the moment, so blinded by desire, that they cannot
-            foresee the pain and trouble that are bound to ensue; and equal
-            blame belongs to those who fail in their duty through weakness of
-            will, which is the same as saying through shrinking from toil and
-            pain. These cases are perfectly simple and easy to distinguish. In a
-            free hour, when our power of choice is untrammelled and when nothing
-            prevents our being able to do what we like best, every pleasure is
-            to be welcomed and every pain avoided. But in certain circumstances
-            and owing to the claims of duty or the obligations of business it
-            will frequently occur that pleasures have to be repudiated and
-            annoyances accepted. The wise man therefore always holds in these
-            matters to this principle of selection: he rejects pleasures to
-            secure other greater pleasures, or else he endures pains to avoid
-            worse pains.
-          </p>
+        <div class="col-12 mt-4" v-html="aboutUs.content">
         </div>
       </div>
     </div>
@@ -47,6 +23,18 @@ export default {
     return {
       title: "CallMyCake | About Us"
     };
+  },
+
+  data() {
+    return {
+      aboutUs: {}
+    };
+  },
+
+  created() {
+    this.$axios
+      .get('api/singletons/get/about_us')
+      .then(({ data }) => this.aboutUs = data);
   }
 };
 </script>
