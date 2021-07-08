55e5ffcc8c0b7020489cf15f3f076695423a8f37
kritish-dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-07-08 10:34:42 +0545

    Added Flavors

--- components/customization/Flavor.vue
+++ components/customization/Flavor.vue
@@ -50,20 +50,20 @@ export default {
   data() {
     return {
       flavors: [
-        {
-          id: 1,
-          image: "/images/customization/flavors/<type>/001.png",
-          icon: "/images/customization/flavor_options/001.png",
-          value: "Top: Whiteforest/Bottom: Whiteforest",
-          rate: 0
-        },
-        {
-          id: 2,
-          image: "/images/customization/flavors/<type>/002.png",
-          icon: "/images/customization/flavor_options/002.png",
-          value: "Top: Chocolate/Bottom: Chocolate",
-          rate: 150
-        },
+        // {
+        //   id: 1,
+        //   image: "/images/customization/flavors/<type>/001.png",
+        //   icon: "/images/customization/flavor_options/001.png",
+        //   value: "Top: White/Bottom: White",
+        //   rate: 0
+        // },
+        // {
+        //   id: 2,
+        //   image: "/images/customization/flavors/<type>/002.png",
+        //   icon: "/images/customization/flavor_options/002.png",
+        //   value: "Top: Chocolate/Bottom: Chocolate",
+        //   rate: 150
+        // },
         // {
         //   id: 3,
         //   image: "/images/customization/flavors/<type>/003.png",
@@ -82,6 +82,76 @@ export default {
           icon: "/images/customization/flavor_options/005.png",
           value: "Top: Red Velvet/Bottom: Red Velvet",
           rate: 400
+        },
+        {
+          id: 6,
+          image: "/images/customization/flavors/<type>/006.png",
+          icon: "/images/customization/flavor_options/006.png",
+          value: "Top: Black Forest/Bottom: Black Forest",
+          rate: 0
+        },
+        {
+          id: 7,
+          image: "/images/customization/flavors/<type>/007.png",
+          icon: "/images/customization/flavor_options/007.png",
+          value: "Top: Blueberry/Bottom: Blueberry",
+          rate: 100
+        },
+        {
+          id: 8,
+          image: "/images/customization/flavors/<type>/008.png",
+          icon: "/images/customization/flavor_options/008.png",
+          value: "Top: Butter Scotch/Bottom: Butter Scotch",
+          rate: 100
+        },
+        {
+          id: 9,
+          image: "/images/customization/flavors/<type>/009.png",
+          icon: "/images/customization/flavor_options/009.png",
+          value: "Top: Chocolate/Bottom: Chocolate",
+          rate: 150
+        },
+        {
+          id: 10,
+          image: "/images/customization/flavors/<type>/010.png",
+          icon: "/images/customization/flavor_options/010.png",
+          value: "Top: Chocolate Truffel/Bottom: Chocolate Truffel",
+          rate: 400
+        },
+        {
+          id: 11,
+          image: "/images/customization/flavors/<type>/011.png",
+          icon: "/images/customization/flavor_options/011.png",
+          value: "Top: Mocha/Bottom: Mocha",
+          rate: 200
+        },
+        {
+          id: 12,
+          image: "/images/customization/flavors/<type>/012.png",
+          icon: "/images/customization/flavor_options/012.png",
+          value: "Top: Oreo/Bottom: Oreo",
+          rate: 200
+        },
+        {
+          id: 13,
+          image: "/images/customization/flavors/<type>/013.png",
+          icon: "/images/customization/flavor_options/013.png",
+          value: "Top: Vanilla/Bottom: Vanilla",
+          rate: 0
+        },
+        {
+          id: 14,
+          image: "/images/customization/flavors/<type>/014.png",
+          icon: "/images/customization/flavor_options/014.png",
+          value: "Top: White Chocolate/Bottom: White Chocolate",
+          rate: 300
+        },
+        {
+          id: 15,
+          image: "/images/customization/flavors/<type>/015.png",
+          icon: "/images/customization/flavor_options/015.png",
+          value: "Top: White Forest/Bottom: White Forest",
+          rate: 0
         }
       ]
     };
